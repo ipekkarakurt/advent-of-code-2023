b1ff38df9584774c853a588b0b15e5ea42c0e8fe
İpek Karakurt <ipekkarakurt@yandex.com>
2023-12-17 21:39:10 +0300

    hello regex my old friend

---
+++ day-2/part_1.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, _ := os.Open("day-2/input.txt")
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+	maxRed := 12
+	maxGreen := 13
+	maxBlue := 14
+	sum := 0
+
+	// 12 red cubes, 13 green cubes, and 14 blue cubes
+	for sc.Scan() {
+		line := sc.Text()
+		possibleGame := true
+
+		s := regexp.MustCompile(`Game (\d*): (.*)`).FindStringSubmatch(line)
+		gameCount := s[1]
+		gamePlay := s[2]
+
+		rounds := strings.Split(gamePlay, "; ")
+
+		for _, round := range rounds {
+			if possibleGame {
+				picks := strings.Split(round, ", ")
+				for _, pick := range picks {
+					splitPick := strings.Split(pick, " ")
+					count, _ := strconv.Atoi(splitPick[0])
+
+					if splitPick[1] == "red" && count > maxRed {
+						possibleGame = false
+						break
+					} else if splitPick[1] == "blue" && count > maxBlue {
+						possibleGame = false
+						break
+					} else if splitPick[1] == "green" && count > maxGreen {
+						possibleGame = false
+						break
+					}
+				}
+			}
+		}
+
+		if possibleGame {
+			c, _ := strconv.Atoi(gameCount)
+			sum += c
+		}
+	}
+	fmt.Println(sum)
+}
---
+++ day-2/part_2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, _ := os.Open("day-2/input.txt")
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+	sum := 0
+
+	// 12 red cubes, 13 green cubes, and 14 blue cubes
+	for sc.Scan() {
+		line := sc.Text()
+
+		s := regexp.MustCompile(`Game (\d*): (.*)`).FindStringSubmatch(line)
+		gamePlay := s[2]
+
+		rounds := strings.Split(gamePlay, "; ")
+
+		minRed := 0
+		minGreen := 0
+		minBlue := 0
+
+		for _, round := range rounds {
+			picks := strings.Split(round, ", ")
+			for _, pick := range picks {
+				splitPick := strings.Split(pick, " ")
+				count, _ := strconv.Atoi(splitPick[0])
+
+				if splitPick[1] == "red" && count > minRed {
+					minRed = count
+				} else if splitPick[1] == "blue" && count > minBlue {
+					minBlue = count
+				} else if splitPick[1] == "green" && count > minGreen {
+					minGreen = count
+				}
+			}
+		}
+		power := minRed * minGreen * minBlue
+		sum += power
+	}
+	fmt.Println(sum)
+}
